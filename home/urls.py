4e390bbf51e798d1376844d79bcf62e5f55da930
ronak6176 <gammers.lion@gmail.com>
2022-11-11 12:08:47 +0530

    hi

--- home/urls.py
+++ home/urls.py
@@ -17,10 +17,16 @@ urlpatterns = [
    path('update/<int:pk>/', views.update_items, name='update-items'),   
    path('item/<int:pk>/delete/', views.delete_items, name='delete-items'),
    # path('', include(router.urls)),
-
+# ###################################################################################33
    path('', views.ApiOverview, name='home'), 
    path('create/appointment', views.add_items, name='add-items'),
    path('all/appointment', views.view_items, name='view_items'),
+   path('update/<int:pk>/', views.update_items, name='update-items'),   
+   path('item/<int:pk>/delete/', views.delete_items, name='delete-items'),
+   #################################################################################
+   path('', views.ApiOverview, name='home'), 
+   path('create/event_benefits', views.add_items, name='add-items'),
+   path('all/event_benefits', views.view_items, name='view_items'),
    path('update/<int:pk>/', views.update_items, name='update-items'),   
    path('item/<int:pk>/delete/', views.delete_items, name='delete-items'),
 ]
--- home/views.py
+++ home/views.py
@@ -5,9 +5,9 @@ from rest_framework.decorators import api_view
 from django.http.response import JsonResponse
 from rest_framework.response import Response
 from rest_framework import serializers,viewsets
-from home.serializers import PersonSerializer,SpeciesSerializer
+from home.serializers import PersonSerializer,SpeciesSerializer,RoSerializer
 from home.models  import user
-from home.models  import appointment
+from home.models  import appointment,event_benefits
 from rest_framework import status
 from django.shortcuts import render
 import pyrebase
@@ -198,6 +198,87 @@ def delete_items(request, pk):
 	return Response(status=status.HTTP_202_ACCEPTED)
 
 
+# **********************************************event_benefits*******************************************************
+
+
+@api_view(['GET'])
+def ApiOverview(request):
+	api_urls = {
+		'all_items': '/all/user',
+		'Search by Category': '/?category=category_name',
+		'Search by Subcategory': '/?subcategory=category_name',
+		'Add': '/create/user',
+		'Update': '/update/pk',
+		'Delete': '/item/pk/delete'
+	}
+
+	return Response(api_urls)
+
+# class PersonViewSet(viewsets.ModelViewSet):
+#    queryset = user.objects.all()
+#    serializer_class = PersonSerializer     
+
+# {
+#         "id": 4,
+#         "name": "ronak",
+#         "city": "rajkot",
+#         "number": "232232323",
+#         "age": "12"
+#     }
+
+
+@api_view(['POST'])
+def add_items(request):
+	item = RoSerializer(data=request.data)
+
+
+	if event_benefits.objects.filter(**request.data).exists():
+		raise serializers.ValidationError('This data already exists')
+
+	if item.is_valid():
+		item.save()
+		return Response(item.data)
+	else:
+		return Response(status=status.HTTP_404_NOT_FOUND)
+
+# class PersonViewSet(viewsets.ModelViewSet):
+#    queryset = user.objects.all()
+#    serializer_class = PersonSerializer 
+
+
+
+@api_view(['GET'])
+def view_items(request):
+    if request.method == 'GET':
+        tutorials = event_benefits.objects.all()
+        
+        title = request.query_params.get('title', None)
+        if title is not None:
+            tutorials = tutorials.filter(title__icontains=title)
+        
+        tutorials_serializer = RoSerializer(tutorials, many=True)
+        return Response(tutorials_serializer.data)
+		# return Response(status=status.HTTP_404_NOT_FOUND)
+
+
+        
+
+@api_view(['POST'])
+def update_items(request, pk):
+	item = event_benefits.objects.get(pk=pk)
+	data = RoSerializer(instance=item, data=request.data)
+
+	if data.is_valid():
+		data.save()
+		return Response(data.data)
+	else:
+		return Response(status=status.HTTP_404_NOT_FOUND)
+
+@api_view(['DELETE'])
+def delete_items(request, pk):
+	item = get_object_or_404(event_benefits, pk=pk)
+	item.delete()
+	return Response(status=status.HTTP_202_ACCEPTED)
 
 
 
